0c5df0897a84914b9fba323dcede5d5f9aabab3a
Erik Nilson <eriknielsen@users.noreply.github.com>
2025-03-08 21:57:36 +0100

    Followed Behaviour Tree tutorial to implement patrol and chase behaviour as  a tree.
    BP_Guard2 should work

--- Source/ue5jam/ue5jamCharacter.cpp
+++ Source/ue5jam/ue5jamCharacter.cpp
@@ -34,7 +34,7 @@ Aue5jamCharacter::Aue5jamCharacter()
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
 	CameraBoom->TargetArmLength = 800.f;
-	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	CameraBoom->SetRelativeRotation(FRotator(-100.f, 0.f, 0.f));
 	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
 	// Create a camera...
